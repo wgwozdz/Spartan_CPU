e9f0e1940e03f9b621a0d909c5bfe693e7915ec5
wgwozdz <wcg3431@gmail.com>
2014-11-30 16:36:28 -0500

    reworked alu itself, now for control.

--- logic_unit.v
+++ logic_unit.v
@@ -1,9 +1,13 @@
 module logic_unit(
+	input clk,
 	input passthrough,
+	input push,
+	input push_high,
 	input add,
 	input sub,
 	input inc,
 	input dec,
+	input mul,
 	input shr,
 	input shl,
 	input band,
@@ -13,23 +17,36 @@ module logic_unit(
 	
 	input [15:0] bus1,
 	input [15:0] bus2,
-	output [15:0] bus3
+	output [15:0] bus3,
+	output [15:0] bus4
 	);
 
-	//TODO: handle overflow flag? maybe assign each clock cycle.
+	//TODO: handle overflow flag?
+	reg [31:0] store;
 	
 	assign bus3 = 
 	passthrough ? bus1 : 
-	bnegate ? ~bus1 :
-	add ? bus1 + bus2 :
-	sub ? bus1 - bus2 :
-	inc ? bus1 + 1 :
-	dec ? bus1 - 1 :
-	band ? bus1 & bus2 :
-	bor ? bus1 | bus2 :
-	bxor ? bus1 ^ bus2:
-	shr ? bus1 >> bus2 :
-	shl ? bus1 << bus2 :
+	push ? store[15:0] :
 	16'bz;
+	
+	assign bus4 = 
+	push_high ? store[31:16] :
+	16'bz;
+
+	always @ (posedge clk) begin
+		store <= 
+		add ? {16'b0, bus1} + {16'b0, bus2} :
+		sub ? {16'b0, bus1} - {16'b0, bus2} :
+		inc ? {16'b0, bus1} + 1 :
+		dec ? {16'b0, bus1} - 1 :
+		mul ? {16'b0, bus1} * {16'b0, bus2} :
+		shr ? {16'b0, bus1} >> bus2 :
+		shl ? {16'b0, bus1} << bus2 :
+		band ? {16'b0, bus1} & {16'b0, bus2} :
+		bor ? {16'b0, bus1} | {16'b0, bus2} :
+		bxor ? {16'b0, bus1} ^ {16'b0, bus2} :
+		bnegate ? ~{16'b0, bus2} :
+		store;
+	end
 
 endmodule
